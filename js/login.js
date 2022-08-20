8edeb8f99127a8bc2d031bee4c681768fb03e3c2
mathiasaristoy <mathias.aristoy@gmail.com>
2022-08-20 18:53:34 -0300

    Entrega 1 casi final otra vez

---
+++ js/login.js
@@ -0,0 +1,28 @@
+function login() {
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if (email === "") {
+        document.getElementById('email').classList.add('is-invalid');
+    }
+    else { document.getElementById('email').classList.remove('is-invalid'); }
+    if (password === "") {
+        document.getElementById('password').classList.add('is-invalid');
+    }
+    else { document.getElementById('password').classList.remove('is-invalid'); }
+    
+    if (email==="" || password==="")
+    return 
+    location.href = 'index.html'
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementById('btn').addEventListener('click', () => {
+        login();
+    })
+})
