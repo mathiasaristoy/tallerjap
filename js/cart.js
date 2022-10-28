6999326960bf99ffbb3b41296a626993cb112caa
mathiasaristoy <mathias.aristoy@gmail.com>
2022-10-28 00:55:25 -0300

    Entrega 6

--- js/cart.js
+++ js/cart.js
@@ -25,14 +25,34 @@ function cart(articles) {
         <th scope="row"><img src="${article.image}" alt="Product image" width="50" height="auto"></th>
         <td>${article.name}</td>
         <td>${article.currency}   <span id="cost">${article.unitCost}</span></td>
-        <td><input id="input" type="number" value="${article.count}" onchange="finalCost(this, ${article.id})" </td>
-        <td><strong id="finalCost">${article.unitCost}</strong></td>
+        <td><input id="input" type="number" min="1" value="${article.count}" onchange="finalCost(this, ${article.id})" </td>
+        <td><strong>${article.currency}</strong> <strong id="finalCost"></strong></td>
       </tr>
       </tr>
     </tbody>
   </table>
     `
   document.getElementById("cart").innerHTML = htmlContentToAppend; 
+
+    const htmlContentToAppend2=
+
+    `<ul class="list-group">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      Subtotal
+      <span id="subtotal">${article.unitCost}</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      Costo de envío
+      <span id="shippingCost"></span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      Total
+      <strong id="finalTotal"></strong>
+    </li>
+  </ul>`
+
+    document.getElementById("costs").innerHTML = htmlContentToAppend2
+  
 
   
 }}
@@ -44,7 +64,18 @@ function cart(articles) {
     let count = event.value ;  
     total = parseInt(cost) * parseInt(count);
     document.getElementById('finalCost').innerHTML = total
+    document.getElementById('subtotal').innerHTML = total
+    
+    let shippingCost = parseFloat(document.querySelector("input[name=shipping]:checked").value);  
+    let percentage = shippingCost * total;
+    document.getElementById('shippingCost').innerHTML = percentage
+   
+    let finalTotal = total + percentage
+    document.getElementById('finalTotal').innerHTML = finalTotal
   }
+
+  
+  
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
@@ -54,6 +85,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
           cart(articles);
       }
   });
-
+ 
 });
+
 
